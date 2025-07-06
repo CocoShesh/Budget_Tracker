53ae6622b56c943f2fa87bd75e3ec3c372585485
Nard <glenardrefamonte1@gmail.com>
2025-07-06 17:38:30 +0800

    first commit

--- src/components/EditBudgetModal.tsx
+++ src/components/EditBudgetModal.tsx
@@ -343,7 +343,6 @@ function EditBudgetModal({ isOpen, onClose, onUpdate, budget, budgets }: EditBud
                 </div>
               )}
 
-              {/* Budget Tip */}
               <div className="bg-green-50 border border-green-200 rounded-lg p-4">
                 <div className="flex items-start">
                   <svg
